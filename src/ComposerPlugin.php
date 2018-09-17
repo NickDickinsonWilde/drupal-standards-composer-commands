f307cb43b6db96cbf497c3e1cbb0358adc66d0f7
Nick Wilde <nick@briarmoon.ca>
2018-09-16 20:12:13 -0700

    Install npm dependencies

--- src/ComposerPlugin.php
+++ src/ComposerPlugin.php
@@ -3,21 +3,29 @@
 namespace NickWilde1990\DrupalStandardsCommands;
 
 use Composer\Composer;
+use Composer\EventDispatcher\EventSubscriberInterface;
 use Composer\IO\IOInterface;
 use Composer\Plugin\Capable;
 use Composer\Plugin\PluginInterface;
+use Composer\Script\Event;
+use Composer\Script\ScriptEvents;
 
 /**
  * Publish capabilities to Composer.
  */
-class ComposerPlugin implements PluginInterface, Capable
+class ComposerPlugin implements PluginInterface, Capable, EventSubscriberInterface
 {
+  /**
+   * @var IOInterface
+   */
+  private $io;
 
-    /**
+  /**
      * {@inheritdoc}
      */
     public function activate(Composer $composer, IOInterface $io)
     {
+        $this->io = $io;
     }
 
     /**
@@ -29,5 +37,33 @@ class ComposerPlugin implements PluginInterface, Capable
             'Composer\Plugin\Capability\CommandProvider' =>
                 'NickWilde1990\DrupalStandardsCommands\Command\Commands',
         ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function getSubscribedEvents()
+    {
+        return [
+            ScriptEvents::POST_INSTALL_CMD => [
+                ['installTestDependencies', 1],
+            ],
+            ScriptEvents::POST_UPDATE_CMD => [
+                ['installTestDependencies', 1],
+            ]
+        ];
+    }
+
+    /**
+     * Install npm based testing dependencies.
+     *
+     * @param Event $event
+     */
+    public function installTestDependencies(Event $event) {
+        $cd = getcwd();
+        chdir('vendor/nickwilde1990/drupal-standards-composer-commands');
+        exec("{$cd}/vendor/bin/npm install");
+        chdir($cd);
+
     }
 }
